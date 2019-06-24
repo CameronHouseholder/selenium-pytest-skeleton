807ab3a41d8ec7f2b9041a98fc9cd39f76c11ea8
Cameron Householder <cameron.householder@ruralsource.com>
2019-06-24 16:17:20 -0500

    added os specific drivers

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # selenium-pytest-skeleton
-Selenium skeleton project using the pytest test framework
+selenium skeleton project using the pytest test framework
 
-# installing required packages
-pip install --user -r requirements.txt
+## installing required packages
+```bash
+pip install -r requirements.txt
+```
 
-# runing the tests
+## running the tests
+Default usage (runs the chromedriver for windows os in headless mode)
+```bash
 pytest --html=test_output/report.html -v -s src/tests
+```
+
+Specific browser and os usage (replace {browser} and replace {os} with desired parameters)
+```bash
+pytest --html=test_output/report.html -v -s src/tests --browser {browser} --os {os}
+```
+Example
+```bash
+pytest --html=test_output/report.html -v -s src/tests --browser firefox --os windows
+```
+
+Browser Options (chrome, chrome_headless, firefox, ie)
+OS Options (windows, mac, linux)
 
 
--- conftest.py
+++ conftest.py
@@ -1,32 +1,59 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.firefox.options import Options
 from datetime import datetime
 
 driver = None
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome_headless")
+    parser.addoption("--os", action="store", default="windows")
 
 @pytest.fixture(scope='class')
 def driver_init(request):
     global driver
     browser = request.config.option.browser
+    os = request.config.option.os
+
+    executable_path_dict  = {
+        "windows": {
+            "chrome": "resources/webdrivers/windows/chromedriver.exe",
+            "firefox": "resources/webdrivers/windows/geckodriver.exe",
+            "ie": "resources/webdrivers/windows/IEDriverServer.exe"
+        },
+        "mac": {
+            "chrome": "resources/webdrivers/mac/chromedriver",
+            "firefox": "resources/webdrivers/mac/geckodriver"
+        },
+        "linux": {
+            "chrome": "resources/webdrivers/linux/chromedriver",
+            "firefox": "resources/webdrivers/linux/geckodriver"
+        }
+    }
     
     if browser == "chrome":
-        chrome_options = Options()
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--incognito")
-        driver = webdriver.Chrome(executable_path="resources/webdrivers/chromedriver.exe", options=chrome_options)
+        options = Options()
+        options.add_argument("--start-maximized")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--incognito")
+        driver = webdriver.Chrome(executable_path=executable_path_dict[os]["chrome"], options=options)
     elif browser == "chrome_headless":
-        chrome_options = Options()
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--incognito")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--incognito")
-        driver = webdriver.Chrome(executable_path="resources/webdrivers/chromedriver.exe", options=chrome_options)
+        options = Options()
+        options.add_argument("--start-maximized")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--incognito")
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        driver = webdriver.Chrome(executable_path=executable_path_dict[os]["chrome"], options=options)
+    elif browser == "firefox":
+        #TODO: Add a firefox specific profile or options for testing
+        driver = webdriver.Firefox(executable_path=executable_path_dict[os]["firefox"])
+        driver.maximize_window()
+    elif browser == "ie":
+        #TODO Add IE specific options for testing, programatically set the zoom level to 100%
+        driver = webdriver.Ie(executable_path=executable_path_dict["windows"]["ie"])
+        driver.maximize_window()
     else: 
         pass
 
